a7e9f44c0bc64eaef43cf409198da03c99c33fed
AbhisekhNayek <115914826+AbhisekhNayek@users.noreply.github.com>
2025-05-16 14:12:02 +0530

    core : Basic frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-4xl font-bold text-red-500">🚀 Tailwind is Working!</h1>
+    </div>
+  )
+}
+
+export default App
